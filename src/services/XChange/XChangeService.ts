f90a160e81eabd4bb0dce52b88d2d0ffaff36bbb
AlexB <6y6jlb@gmail.com>
2023-02-03 11:10:34 +0400

    git ignore fix

--- src/services/XChange/XChangeService.ts
+++ src/services/XChange/XChangeService.ts
@@ -1,26 +1,36 @@
-import moment from 'moment';
 import fs from 'fs';
-import { EXchangeError } from './../../exceptions/Exchange';
+import moment from 'moment';
 import XChangeClient from "../../dal/XChange/Client";
-import { IOpeneXChangeRatesLatestGet, IOpeneXChangeRatesLatestGetRate } from "../../dal/XChange/types";
+import { IOpeneXChangeRatesLatest, IOpeneXChangeRatesLatestGet, IOpeneXChangeRatesLatestGetRate } from "../../dal/XChange/types";
+import { EXchangeError } from './../../exceptions/Exchange';
 
 class XChangeService {
     private async get(data?: IOpeneXChangeRatesLatestGet) {
-        return await XChangeClient.get(data);
+        const result = await XChangeClient.get(data);
+        fs.writeFile('storage/exchangeRate.json', JSON.stringify(result), function (error) {
+            if (error) console.warn(error.message);
+            console.log('Rate was writed successfully.');
+        });
+        return result;
     }
-    
+
 
     async getRate(data: IOpeneXChangeRatesLatestGetRate): Promise<number> {
 
-        const oldRates = await fs.promises.readFile('storage/exchangeRate.json', 'utf-8');
-        let result = JSON.parse(oldRates.toString())
-        if (result.timestamp && moment.unix(result.timestamp).diff(moment(), 'hours') >= 4) {
+        let result: IOpeneXChangeRatesLatest;
+
+        try {
+            const oldRates = await fs.promises.readFile('storage/exchangeRate.json', 'utf-8');
+
+            result = JSON.parse(oldRates.toString())
+            if (result.timestamp && moment.unix(result.timestamp).diff(moment(), 'hours') >= 4) {
+                result = await this.get();
+            }
+        } catch (error) {
+            console.warn(error.message)
             result = await this.get();
-            fs.writeFile('storage/exchangeRate.json', JSON.stringify(result), function (err) {
-                if (err) throw err;
-                console.log('Rate was writed successfully.');
-            });
         }
+
 
         const usdRateCurrent = result.rates[data.current.toUpperCase()];
         const usdRateTarget = result.rates[data.target.toUpperCase()];
@@ -29,7 +39,7 @@ class XChangeService {
         }
 
         const relation = usdRateCurrent / usdRateTarget;
-        const rate = relation < 0.2 ? relation.toFixed(2) : relation.toFixed(3);
+        const rate = relation < 0.2 ? relation.toFixed(3) : relation.toFixed(2);
         return +rate;
     }
 
