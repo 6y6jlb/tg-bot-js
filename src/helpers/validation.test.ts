37e9062728714b82606e9f34f7779d19ea062559
AlexB <6y6jlb@gmail.com>
2023-02-26 10:54:34 +0400

    more test for regex

--- src/helpers/validation.test.ts
+++ src/helpers/validation.test.ts
@@ -1,3 +1,4 @@
+import { EXchangeError } from './../exceptions/Exchange';
 import { TaskError, CreateTaskError } from './../exceptions/Task';
 import { describe, expect, test } from '@jest/globals';
 import { exhangeRequestValidation, isNumeric, taskCreationValidator, taskTimeValidator } from './validation';
@@ -16,13 +17,53 @@ describe('validation', () => {
     });
 
     test('it fail validate task creation vol.1', () => {
-        expect(()=> taskCreationValidator('20:00- Moscow')).toThrow(CreateTaskError);
-        expect(()=> taskCreationValidator('20:00- Moscow')).toThrow('20:00- Moscow');
+        expect(() => taskCreationValidator('20:00- Moscow')).toThrow(CreateTaskError);
+        expect(() => taskCreationValidator('20:00- Moscow')).toThrow('Message validation error ( 20:00- Moscow )');
     });
 
     test('it fail validate task creation vol.2', () => {
-        expect(()=> taskCreationValidator('20:00 Moscow - Europe/Moscow')).toThrow(CreateTaskError);
-        expect(()=> taskCreationValidator('20:00 Moscow - Europe/Moscow')).toThrow('20:00 Moscow - Europe/Moscow');
+        expect(() => taskCreationValidator('20:00 Moscow - Europe/Moscow')).toThrow(CreateTaskError);
+        expect(() => taskCreationValidator('20:00 Moscow - Europe/Moscow')).toThrow('Message validation error ( 20:00 Moscow - Europe/Moscow )');
+    });
+
+    test('it validate correct string for task time vol.1', () => {
+        expect(() => taskTimeValidator('20:00')).toBeTruthy();
+    });
+
+
+    test('it validate correct string for task time vol.2', () => {
+        expect(() => taskTimeValidator('00:00')).toBeTruthy();
+    });
+
+
+    test('it fail validate task time vol.1', () => {
+        expect(taskTimeValidator('00:99')).toBeFalsy();
+    });
+
+    test('it fail validate task time vol.2', () => {
+        expect(taskTimeValidator('00:001')).toBeFalsy();
+    });
+
+    test('it fail validate task time vol.3', () => {
+        expect(taskTimeValidator('00-99')).toBeFalsy();
+    });
+
+    test('it validate correct exchange task string vol.1', () => {
+        expect(exhangeRequestValidation('100 usd rub')).toEqual({ "count": 100, "current": "RUB", "target": "USD" });
+    });
+
+    test('it validate correct exchange task string vol.2', () => {
+        expect(exhangeRequestValidation('usd rub')).toEqual({ "count": 1, "current": "RUB", "target": "USD" });
+    });
+
+    test('it fail validate exchange string vol.1', () => {
+        expect(() => exhangeRequestValidation('usd - rub')).toThrow(EXchangeError);
+        expect(() => exhangeRequestValidation('usd - rub')).toThrow('Message validation error ( usd - rub )');
+    });
+
+    test('it fail validate exchange string vol.2', () => {
+        expect(() => exhangeRequestValidation('usd')).toThrow(EXchangeError);
+        expect(() => exhangeRequestValidation('usd')).toThrow('Message validation error ( usd )');
     });
 
 });
