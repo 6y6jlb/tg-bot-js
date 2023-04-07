7b0049554718ddac4e452981d4e5e8a2096f22fb
AlexB <6y6jlb@gmail.com>
2023-04-07 16:04:25 +0400

    delete tasks simple

--- src/services/telegramHandlers/commands/deleteTask.ts
+++ src/services/telegramHandlers/commands/deleteTask.ts
@@ -2,10 +2,44 @@ import { i18n } from "i18next";
 import { APP_TYPE_ENUM } from "../../../models/const";
 import { Notification } from "../../Notification/Abstract";
 import UserSettingsService from "../../UserSetttings/UserSettingsService";
+import TaskService from "../../Task/TaskService";
+import AdminService from "../../Admin/AdminService";
+import { COMMANDS } from "../../../utils/const";
+import { ITask } from "../../../models/types";
+import { Callback } from "../../Notification/Callback";
 
 
-export async function deleteTask(notification: Notification, i18: i18n) {
+export async function deleteTask(notification: Callback, i18: i18n) {
   const chatId = notification.getChatId();
-  UserSettingsService.updateOrCreate({ user_id: chatId, app_type: APP_TYPE_ENUM.TASK_DELETE, created_at: new Date() });
-  await notification.send({ text: `${i18.t('actions.tasks.delete-description')}` });
+  const data = notification.getData();
+  const params = new URLSearchParams(data.split('?')[1]);
+  const taskId = params.has('task_id') && params.get('task_id');
+  const buttons = [];
+  let message = '';
+  
+  if (taskId) {
+    
+    message = i18.t('tasks.delete.success');
+    await TaskService.delete({ _id: taskId });
+    
+  } else {
+
+    const isAdmin = AdminService.checkAdmin(chatId);
+    const tasks = await TaskService.get(isAdmin ? {} : { user_id: chatId }) as ITask[];
+
+    tasks.forEach(task => buttons.push([{ text: task._id, callback_data: `${COMMANDS.TASKS_DELETE}?task_id=${task._id}` }]))
+    message = i18.t('actions.tasks.delete-description');
+
+  }
+ 
+  buttons.push([{ text: i18.t('buttons.reset'), callback_data: COMMANDS.RESTART }])
+
+  await notification.send({
+    text: message, options: {
+      parse_mode: 'HTML',
+      reply_markup: {
+        inline_keyboard: buttons
+      }
+    }
+  });
 }
