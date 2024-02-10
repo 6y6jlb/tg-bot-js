6ddbbc14a1858d63af546eb636fe7c9fe7cf9795
AlexB <6y6jlb@gmail.com>
2024-02-10 11:47:37 +0400

    task schema, validation rules correction

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,14 +1,25 @@
 import mongoose, { Schema } from "mongoose";
+import { EVENT_ENUM } from "./const";
 import { ITask } from './types';
 
-const Task = new mongoose.Schema<ITask>({
+const TaskSchema = new mongoose.Schema<ITask>({
     user_id: { type: Schema.Types.ObjectId, required: true },
     last_call: { type: Date, required: false },
-    options: { type: [], required: true, default: [] },
+    options: {
+        type: [
+            {
+                event_type: { type: String, enum: EVENT_ENUM, required: true },
+                param: { type: String }
+            }
+        ],
+        required: true,
+        default: []
+    },
     call_at: { type: String, required: true },
     tz: { type: String, required: false, default: 'Europe/Moscow' },
     is_regular: { type: Boolean, required: false, default: false },
     queue: { type: Boolean, required: false, default: false },
-})
+});
 
-export default mongoose.model('Task', Task);
+export default mongoose.model<ITask>('Task', TaskSchema);
+
--- src/requests/Task/schema.ts
+++ src/requests/Task/schema.ts
@@ -1,4 +1,5 @@
 import Joi from 'joi';
+import { EVENT_ENUM } from '../../models/const';
 
 
 export const getSchema = Joi.object({
@@ -6,19 +7,25 @@ export const getSchema = Joi.object({
 });
 
 export const storeSchema = Joi.object({
-    user_id: Joi.string().required(),
+    user_id: Joi.string().allow(null),
     tz: Joi.string().min(3).max(50),
     call_at: Joi.string().required(),
-    is_reqular: Joi.bool(),
-    options: Joi.array(),
+    is_regular: Joi.boolean(),
+    options: Joi.array().items(Joi.object({
+        event_type: Joi.string(),
+        param: Joi.string()
+    })),
 });
 
 export const updateSchema = Joi.object({
     task_id: Joi.string().required(),
     tz: Joi.string().min(3).max(50),
     call_at: Joi.string().required(),
-    is_reqular: Joi.boolean(),
-    options: Joi.array(),
+    is_regular: Joi.boolean(),
+    options: Joi.array().items(Joi.object({
+        event_type: Joi.string(),
+        param: Joi.string()
+    })),
 })
 
 
